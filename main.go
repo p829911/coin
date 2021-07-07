0811d306914cde01e94ead4024d0f84611a83f74
p829911 <p829911@gmail.com>
2021-07-07 14:51:05 +0900

    refactoring singleton pattern

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "github.com/p829911/coin/blockchain"
+import (
+	"fmt"
+	"github.com/p829911/coin/blockchain"
+)
 
 func main() {
 	chain := blockchain.GetBlockchain()
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
+	chain.AddBlock("Fourth Block")
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %s\n", block.Hash)
+		fmt.Printf("PrevHash: %s\n", block.PrevHash)
+	}
 }
